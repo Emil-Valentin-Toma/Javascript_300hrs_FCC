8d67053724d1ef0a5eb7d879c257fb9ef2d89067
Emil V. Toma <evtoma@gmail.com>
2020-04-12 13:19:35 +0100

    arrays

---
+++ access-array-data-with-indexes.js
@@ -0,0 +1,8 @@
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/access-array-data-with-indexes
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line
+var myData = myArray[0];
+console.log(myData);
---
+++ nest-one-array-within-another-array.js
@@ -0,0 +1,3 @@
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/nest-one-array-within-another-array
+var myArray = [["tiny dogs", 23], ["big cats", 53], ["milky cows", 7], ["green zebras", 4]];
+console.log(myArray);
