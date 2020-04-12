c1f404354f9ce842189cc1015652ab619b7ce664
Emil V. Toma <evtoma@gmail.com>
2020-04-12 12:39:15 +0100

    funny word -blanks

---
+++ word-blanks.js
@@ -0,0 +1,13 @@
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/word-blanks
+
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+
+// Only change code below this line
+var wordBlanks = "Tiny"+" " + myAdjective +" " + myNoun +" " +"bits"+ " " + myVerb + " " + myAdverb; 
+console.log(wordBlanks);
+// Change this line
+// Only change code above this line
